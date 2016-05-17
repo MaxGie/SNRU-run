0e2b540956b87eb5fb425b7531046c92191dc3a9
Wasan <maxgie1648@gmail.com>
2016-05-17 14:55:35 -0700

    เสร๊จสิ้น

---
+++ app/src/main/java/snru/srisuk/wasan/snrurun/MyManage.java
@@ -0,0 +1,20 @@
+package snru.srisuk.wasan.snrurun;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Administrator on 17/5/2559.
+ */
+public class MyManage {
+
+
+    //Explicit
+    private MyOpenHelper myOpenHelper;
+    private SQLiteDatabase sqLiteDatabase;
+
+    public MyManage(Context context) {
+        myOpenHelper = new MyOpenHelper(context);
+        sqLiteDatabase = myOpenHelper.getWritableDatabase();
+    }//Constructor
+}// Main Class
