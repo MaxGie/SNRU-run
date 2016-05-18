1a72632bb313d369dbdafe828948bd7333986a71
Wasan <maxgie1648@gmail.com>
2016-05-17 18:00:39 -0700

    จบวันที่ 17 พ.ค.

--- app/src/main/java/snru/srisuk/wasan/snrurun/MainActivity.java
+++ app/src/main/java/snru/srisuk/wasan/snrurun/MainActivity.java
@@ -2,19 +2,39 @@ package snru.srisuk.wasan.snrurun;
 
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
     private MyManage myManage;
+    private ImageView imageView;
+    private EditText userEditText, passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        imageView = (ImageView) findViewById(R.id.imageView6);
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passwordEditText = (EditText) findViewById(R.id.editText5);
+
 
         myManage = new MyManage(MainActivity.this);
 
@@ -24,7 +44,66 @@ public class MainActivity extends AppCompatActivity {
         //Delete All SQLite
         DeleteAllSQLite();
 
+        //Synchronize
+        MySynchronize mySynchronize = new MySynchronize();
+        mySynchronize.execute();
+
+        //Show Logo
+
+        Picasso.with(MainActivity.this)
+                .load("http://swiftcodingthai.com/snru/image/logo_snru.png")
+                .into(imageView);
+
     }//Main Method
+
+    //Create Inner Class
+
+    public class MySynchronize extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url("http://swiftcodingthai.com/snru/get_user.php").build();
+                Response response = okHttpClient.newCall(request).execute();
+
+                return response.body().string();
+
+            } catch (Exception e) {
+                return null;
+            }
+
+           // return null;
+        }//doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("Snru", "JSON ===> " + s);
+
+            try {
+                JSONArray jsonArray = new JSONArray(s);
+                for (int i=0;i<jsonArray.length();i++) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    String strName = jsonObject.getString(MyManage.column_name);
+                    String strUser = jsonObject.getString(MyManage.column_user);
+                    String strPassword = jsonObject.getString(MyManage.column_password);
+                    String strAvata = jsonObject.getString(MyManage.column_avata);
+
+                    myManage.addUser(strName, strUser, strPassword, strAvata);
+
+                }//for
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }// onPost
+    }//MySyn Class
+
+
 
     private void DeleteAllSQLite() {
         SQLiteDatabase sqLiteDatabase=openOrCreateDatabase(MyOpenHelper.database_name,
